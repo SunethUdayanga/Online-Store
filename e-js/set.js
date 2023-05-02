fda201338d09f575ce1e0d25719fae835da05bf8
Suneth Udayanga <sunethcreation44@gmail.com>
2023-05-02 15:07:31 +0530

    add backend of store
    add js raw code
    not styling
    
    Co-Authored-By: Hansi04 <113275242+Hansi04@users.noreply.github.com>
    Co-Authored-By: rashmika3433 <65844409+rashmika3433@users.noreply.github.com>
    Co-Authored-By: Ushmika Kavishan <91392668+Ushmika@users.noreply.github.com>

--- e-js/set.js
+++ e-js/set.js
@@ -1,32 +1,69 @@
-
-fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRwA7ScYW-qlcurArWdWQcEu6T8XjCys9IItTUjKoUqCqhTfgs9lyyBoFyKl7IlUwGTda24_0xZ_g4s/pub?output=csv")
-    .then(response => response.text())
-    .then(text => {
+var a = fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vRwA7ScYW-qlcurArWdWQcEu6T8XjCys9IItTUjKoUqCqhTfgs9lyyBoFyKl7IlUwGTda24_0xZ_g4s/pub?output=csv')
+  .then(response => response.text())
+  .then(text => {
+    const rows = text.split('\n');
+    const data = rows.map(row => row.split(','));
+    var dbody= document.getElementById("up-cbody").getElementsByClassName("cbody")[0];
+    dbody.innerHTML = "";
+    var idvalue = 0;
+    for (var row = 1; row < rows.length; row++) {
+        idvalue++;
+        const box = document.createElement("div");
+        box.id = "card"+idvalue;
+        box.className = "card";
+        document.getElementById("cbody").appendChild(box);
         
-        const rows = text.split('\n');
-        const data = rows.map(row => row.split(','));
+        var rowColData = rows[row].split(',');
 
-        var m_grid = document.getElementById("ul-grid").getElementsByTagName("section")[0];
-        m_grid.innerHTML = "";
+        const cin = document.createElement("div");
+        cin.className = "card-info";
+        cin.id = "card-info"+idvalue;
+        document.getElementById("card"+idvalue).appendChild(cin);
 
-        for (var row = 1; row < rows.length; row++) {
-            var newRow = m_grid.insertRow();
+        const cimg = document.createElement("img");
+        cimg.className ="c-img";
+        var ilink = rowColData[6];
+        cimg.src = ilink.replace("https://drive.google.com/open?id=","https://drive.google.com/uc?export=view&id=");
+        document.getElementById("card-info"+idvalue).appendChild(cimg);
 
-            rowColData = rows[row].split(',');
+        const ttlp = document.createElement("p");
+        ttlp.className ="title";
+        ttlp.innerText = rowColData[1];
+        document.getElementById("card-info"+idvalue).appendChild(ttlp);
 
-            for (var col = 0; col < rowColData.length; col++) {
-     
-                // Insert a cell at the end of the row
-                var newCell = newRow.insertCell();
-                newCell.innerHTML = rowColData[col];
+        const ttlpb = document.createElement("p");
+        ttlpb.className ="i-price";
+        ttlpb.innerText = rowColData[4];
+        document.getElementById("card-info"+idvalue).appendChild(ttlpb);
 
-           }
+        const cdbtn = document.createElement("button");
+        cdbtn.className = "btn-det";
+        cdbtn.innerHTML = "Details";
+        document.getElementById("card-info"+idvalue).appendChild(cdbtn);
 
-        }
+        const cpbtn = document.createElement("button");
+        cpbtn.className = "btn-cart";
+        cpbtn.innerHTML = "To Cart";
+        document.getElementById("card-info"+idvalue).appendChild(cpbtn);
 
 
-        console.log(newRow);
 
 
+        
 
-    });
+        console.log(rowColData[5]);
+        
+        
+        
+
+
+    }
+
+    
+    console.log(dbody);
+    
+
+  
+  });
+
+
